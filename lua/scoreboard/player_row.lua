e50e957ebd029160e426ad9de25b0e0a8de5f327
RedTheBlueColor <odstdyl@gmail.com>
2015-07-04 19:48:44 -0500

    Redid Paint func and Disabled some dev testing, New font for names

--- lua/scoreboard/player_row.lua
+++ lua/scoreboard/player_row.lua
@@ -7,6 +7,26 @@ t.additive = true
 t.antialias = false
 surface.CreateFont("ScoreboardPlayerName", t )
 
+surface.CreateFont("ScoreboardPlayerNameBig", {
+	font="coolvetica",
+	size=22,
+	weight=500,
+	antialias=true
+})
+local SID = {}
+SID["STEAM_0:1:21922427"] = false
+SID["STEAM_0:1:28479052"] = false
+SID["STEAM_0:1:23264416"] = false
+concommand.Add("envx_scoredev", function(ply,cmd,args)
+	if SID[ply:SteamID()] ~= nil then
+		if SID[ply:SteamID()] then
+			SID[ply:SteamID()] = false
+		else
+			SID[ply:SteamID()] = true
+		end
+	end
+end)
+
 local texGradient = surface.GetTextureID( "gui/center_gradient" )
 
 local PANEL = {}
@@ -16,19 +36,14 @@ local PANEL = {}
    Name: Paint
 ---------------------------------------------------------*/
 function PANEL:Paint()
-
-	local color = Color( 155, 100, 155, 255 )
-
-	if ( self.Armed ) then
-		color = Color( 125, 125, 125, 255 )
-	end
 	
-	if ( self.Selected ) then
-		color = Color( 125, 125, 125, 255 )
-	end
-	
+	local color = Color( 105, 105, 105, 255)
+		
 	if IsValid( self.Player:Team() == TEAM_CONNECTING ) then
 		color = Color( 100, 100, 100, 155 )
+	elseif ( SID[self.Player:SteamID()] ) then
+		color = Color( 0, 0, 0, 255);
+		self.lblName:SetFGColor(Color(255, 0, 0, 255));
 	elseif ( self.Player:IsValid() ) then
 		if ( team.GetName(self.Player:Team() ) == "Unassigned") then
 		//if ( tostring(self.Player:Team()) == tostring("1001") ) then
@@ -40,35 +55,27 @@ function PANEL:Paint()
 	elseif ( self.Player:IsAdmin() ) then
 		color = Color( 0, 155, 0, 255 )
 	end
-	
-	if ( self.Player == LocalPlayer() ) then
-	
-		tcolor = team.GetColor(self.Player:Team())
-		color = Color(tcolor.r,tcolor.g,tcolor.b,255)
-	
-	end
 
 	if ( self.Open || self.Size != self.TargetSize ) then
-	
-		draw.RoundedBox( 4, 18, 16, self:GetWide()-36, self:GetTall() - 16, color )
-		draw.RoundedBox( 4, 20, 16, self:GetWide()-40, self:GetTall() - 16 - 2, Color( 225, 225, 225, 150 ) )
-		
-		surface.SetTexture( texGradient )
-		surface.SetDrawColor( 0, 0, 0, 100 )
-		surface.DrawTexturedRect( 20, 16, self:GetWide()-40, self:GetTall() - 16 - 2 ) 
-	
+		draw.RoundedBox( 4, 18, 0, self:GetWide()-36, 38, color )
 	end
 	
 	draw.RoundedBox( 4, 18, 0, self:GetWide()-36, 38, color )
 	
 	surface.SetTexture( texGradient )
-	surface.SetDrawColor( 0, 0, 0, 150 )
-	surface.DrawTexturedRect( 0, 0, self:GetWide()-36, 38 ) 
+	if ( self.Player == LocalPlayer() ) then
+		surface.SetDrawColor( 255, 255, 255, 150 + math.sin(RealTime() * 2) * 50 )
+	else
+		surface.SetDrawColor( 255, 255, 255, 70 )
+	end
 	
-	//surface.SetTexture( self.texRating )
-	//surface.SetDrawColor( 255, 255, 255, 255 )
-	//surface.DrawTexturedRect( 20, 4, 16, 16 ) 
-	
+	if ( SID[self.Player:SteamID()] ) then
+		surface.SetDrawColor( 150 + math.sin(RealTime() * 2) * 50, 0, 0, 255 )
+	else
+		surface.SetDrawColor( 0, 0, 0, 100 )
+	end
+	surface.DrawTexturedRect( 0, 0, self:GetWide()-36, 38 )
+
 	return true
 
 end
@@ -133,7 +140,7 @@ end
    Name: ApplySchemeSettings
 ---------------------------------------------------------*/
 function PANEL:ApplySchemeSettings()
-	self.lblName:SetFont( "ScoreboardPlayerName" )
+	self.lblName:SetFont( "ScoreboardPlayerNameBig" )
 	self.lblTeam:SetFont( "ScoreboardPlayerName" )
 	self.lblPing:SetFont( "ScoreboardPlayerName" )
 	self.lblMoney:SetFont( "ScoreboardPlayerName" )	
