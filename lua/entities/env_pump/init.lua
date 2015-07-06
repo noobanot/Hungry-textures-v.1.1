5b7df9e40f2dd16c145e0b8105212d27918b3b3f
MechanosG <tr0ubl3mak3r@yahoo.com>
2015-07-06 05:15:37 -0700

    Hopefully fully working pump

--- lua/entities/env_pump/init.lua
+++ lua/entities/env_pump/init.lua
@@ -33,11 +33,13 @@ function ENT:Initialize()
 	
 	self.MyPlug = nil
 	self.TheirPlug = nil
-	self.Pumping = false
-	self.Receiving = false
+	self.Weld = nil
 	self.FlowRate = 256	--Default
 	self.Toggled = false
 	self.ReelIn = false
+	self.AttachStep = 0
+	self.InUse = false
+	self.AttachCooldown = CurTime()
 end
 
 function ENT:Setup( pump, rate, hoselength )		--for the toolgun
@@ -49,16 +51,53 @@ function ENT:Setup( pump, rate, hoselength )		--for the toolgun
 	self:SupplyResource("energy", 0)
 end
 
+function ENT:CreateHose()
+	if not IsValid(self.MyPlug) then return end
+	if IsValid(self.Hose) then self.Hose:Remove() end
+	if IsValid(self.rope) then self.rope:Remove() end
+	
+	local LPos1 = Vector(5,13,10)
+	local LPos2 = Vector(10,0,0)
+	local width = 3
+	local material = "cable/cable2"
+		
+	self.Hose, self.rope = constraint.Elastic( self, self.MyPlug, 0, 0, LPos1, LPos2, 500, 0, 0, material, width, true )
+		local ctable = {
+			Type 		= "LSWinch",
+			pl			= self:GetPlayer(),
+			Ent1		= self,
+			Ent2		= self.MyPlug,
+			Bone1		= Bone1,
+			Bone2		= Bone2,
+			LPos1		= LPos1,
+			LPos2		= LPos2,
+			width		= width,
+			material	= material
+		}
+		self.rope.Type = "" --prevents the duplicator from making this weld
+		self.Hose:SetTable( ctable )
+		self.MyPlug:DeleteOnRemove( self.Hose )
+		self:DeleteOnRemove( self.Hose )
+		self.Hose:DeleteOnRemove( self.rope )
+		
+		local dist = (self.MyPlug:GetPos() - self:GetPos()):Length()
+		self.ropelength = math.min(dist+50,self.ropemax)
+		
+		self.Hose:Fire("SetSpringLength", self.ropelength, 0)
+		self.rope:Fire("SetLength", self.ropelength, 0)
+		--self.ropemax = (self.hose_length*100)
+		
+		--print(type(self.Hose).." "..type(self.rope))
+end
+
 function ENT:CreatePlug()
-	if not self.Receiving and not IsValid(self.MyPlug) then
+	if not IsValid(self.Weld) and not IsValid(self.MyPlug) then
 		
 		self:EmitSound( "Buttons.snd17" )
 		
 		--First, we create the plug at a local position
 		local LPos1 = Vector(5,13,10)
 		local LPos2 = Vector(10,0,0)
-		local width = 3
-		local material = "cable/cable2"
 		local pos = self:LocalToWorld( Vector(15,13,10) )
 		local ang = self:GetAngles() + Angle(180,0,0)
 	
@@ -72,79 +111,65 @@ function ENT:CreatePlug()
 		local phys = plug:GetPhysicsObject()
 			phys:EnableGravity( true )
 			phys:EnableMotion( true )
-			phys:SetVelocity(self:GetForward() * 50)
+			phys:SetVelocity(self:GetForward() * 10)
 			phys:Wake()
 		
 		self.MyPlug = plug
 		
-		--Now we set the plug up and give it ropes
+		--Now we set the plug up and give it a hose
 		self.MyPlug.is_plug = true
 		self.MyPlug.MySocket = self
 		self.MyPlug.TheirSocket = nil
 		self.MyPlug.Weld = nil
 		self.MyPlug:SetVar('Owner',self:GetPlayer())
 		
-		--self.nocollide = constraint.NoCollide( self, plug, 0, 0 )
-		self.Hose, self.rope = constraint.Elastic( self, plug, 0, 0, LPos1, LPos2, 500, 0, 0, material, width, true )
-		local ctable = {
-			Type 		= "LSWinch",
-			pl			= self:GetPlayer(),
-			Ent1		= self,
-			Ent2		= plug,
-			Bone1		= Bone1,
-			Bone2		= Bone2,
-			LPos1		= LPos1,
-			LPos2		= LPos2,
-			width		= width,
-			material	= material
-		}
-		self.rope.Type = "" --prevents the duplicator from making this weld
-		--self.nocollide.Type = "" --prevents the duplicator from making this weld
-		self.Hose:SetTable( ctable )
-	
-		plug:DeleteOnRemove( self.Hose )
-		--plug:DeleteOnRemove( self.nocollide )
-		self:DeleteOnRemove( self.Hose )
 		self:DeleteOnRemove( self.MyPlug )
+		
+		self:CreateHose()
+		
+		--self.nocollide = constraint.NoCollide( self, plug, 0, 0 )
+		--self.nocollide.Type = "" --prevents the duplicator from making this weld
+		--plug:DeleteOnRemove( self.nocollide )
 		--self.Hose:DeleteOnRemove( self.nocollide )
-		self.Hose:DeleteOnRemove( self.rope )
-	
-		self.ropelength = 50
-		self.ropemax = (self.hose_length*100)
-	
-		--print(type(self.Hose).." "..type(self.rope))
+
 		if WireAddon then Wire_TriggerOutput(self, "Deployed", 1) end
 	end
 end
 
 function ENT:Disconnect()
-	if IsValid(self.MyPlug) then
-		self:EmitSound( "Buttons.snd17" )
+	self:EmitSound( "Buttons.snd17" )
+	--First, check if we have a plug attached to us, if yep, remove
+	local weldremoved = false
+	if IsValid(self.Weld) then
+		self.Weld:Remove()
+		weldremoved = true
+	end
+		
+	--Next, see if their plug exists
+	if IsValid(self.TheirPlug) then
+			--if we removed a weld, kick plug away
+			if weldremoved then
+				local phys = self.TheirPlug:GetPhysicsObject()
+				phys:EnableGravity( true )
+				phys:EnableMotion( true )
+				phys:SetVelocity(self:GetForward() * 10)
+				phys:Wake()
+			end
+				
+			--Set variables so they don't see us and we don't see them
+			self.TheirPlug.TheirSocket = nil
+			self.TheirPlug = nil
+			self.AttachCooldown = CurTime()+1
+		
+	--No plug detected, lets see if we have our own plug out and bring it in
+	elseif IsValid(self.MyPlug) then
 		self.ReelIn=true
-	else
 	end
 end
 
-function ENT:Think()
-	self.BaseClass.Think(self)
-	
-	--If we have a plug out, lets do stuff
-	if IsValid(self.MyPlug) then
-		
-		
-		--First, some weld checking to make sure nothing disconnected
-		if IsValid(self.MyPlug.TheirSocket) and not IsValid(self.MyPlug.Weld) then
-			self.MyPlug.TheirSocket = nil
-			self.Pumping = false
-		end
-		if IsValid(self.TheirPlug) and not IsValid(self.TheirPlug.Weld) then
-			self.TheirPlug = nil
-		end
-		
-			
-		--See if we are connected to a socket
-		if IsValid(self.MyPlug.TheirSocket) then
-			self.Pumping = true
+function ENT:PumpCheck()
+	if IsValid(self.MyPlug) and IsValid(self.MyPlug.TheirSocket) then -- If we are connected, try to transfer resources
+		if IsValid(self.MyPlug.Weld) and IsValid(self.node) then
 			--First see if we have the energy to power the pump
 			local energyneeded = math.abs(math.floor(self.FlowRate / 100 * Energy_Increment))
 			if energyneeded > 0 and self.FlowRate > 0 and self:GetResourceAmount("energy") >= energyneeded then
@@ -162,42 +187,58 @@ function ENT:Think()
 					end
 				end
 			end
-		else
-			self.Pumping = false
+		end
+	end
+end
+
+function ENT:Think()
+	self.BaseClass.Think(self)
+	
+	--Weld checking, if we see their plug but don't see their weld, run disconnect
+	if IsValid(self.TheirPlug) and (not IsValid(self.TheirPlug.Weld) or not IsValid(self.Weld)) and self.AttachStep==0 then self:Disconnect() end
+	
+	--If we have a plug out, lets do stuff
+	if IsValid(self.MyPlug) then
+		
+		-- First, if we have a plug out and it was disconnected from a socket, make sure other socket cleaned up variables
+		if IsValid(self.MyPlug.TheirSocket) and not IsValid(self.MyPlug.Weld) then
+			self.MyPlug.TheirSocket = nil
 		end
 		
+		self:PumpCheck() --Pump Resources if we can
+		
 		--Not connected yet, lets play with the hoses!
-		if not self.ReelIn and not self.Pumping then		
+		if not self.ReelIn and not IsValid(self.MyPlug.Weld) and self.AttachStep == 0 then		
 			local dist = (self:GetPos() - self.MyPlug:GetPos()):Length()
 			if (self.ropelength <= self.ropemax) and (dist > self.ropelength - 32 ) then
-				self.ropelength = self.ropelength + 50
-				if (self.Hose and self.Hose:IsValid()) then
+				self.ropelength = self.ropelength + 100
+				if IsValid(self.Hose) then
 					self.Hose:Fire("SetSpringLength", self.ropelength, 0)
 				else
 					self.ropemax = 0
 					self.ropelength = 0
 				end
-				if (self.rope and self.rope:IsValid()) then
+				if IsValid(self.rope) then
 					self.rope:Fire("SetLength", self.ropelength, 0)
 				else
 					self.ropemax = 0
 					self.ropelength = 0
 				end
 			end
-		elseif self.ReelIn then
+		elseif self.ReelIn and self.AttachStep == 0 then
 			if IsValid(self.MyPlug.Weld) then
 				self.MyPlug.Weld:Remove()
 			end
 			if (self.ropelength > 0) then
-				if (self.ropelength > 200) then --reel in faster
-					self.ropelength = self.ropelength - 20
+				if (self.ropelength > 80) then --reel in faster
+					self.ropelength = self.ropelength - 40
 				elseif (self.ropelength > 0) then
-					self.ropelength = self.ropelength - 10
+					self.ropelength = self.ropelength - 20
 					if self.ropelength < 0 then self.ropelength = 0 end
 				else
 					self.ropelength = 0
 				end
-				if (self.Hose:IsValid() and self.rope:IsValid())then
+				if IsValid(self.Hose) and IsValid(self.rope) then
 					self.Hose:Fire("SetSpringLength", self.ropelength, 0)
 					self.rope:Fire("SetLength", self.ropelength, 0)
 				else
@@ -212,6 +253,7 @@ function ENT:Think()
 				if WireAddon then Wire_TriggerOutput(self, "Deployed", 0) end
 			end
 		end
+		
 	elseif not IsValid(self.TheirPlug) then
 		-- Search for plugs to attach to us
 		local sockCenter = self:LocalToWorld( Vector(5,13,10) )
@@ -225,39 +267,15 @@ function ENT:Think()
 		end
 	end
 	
-	if IsValid(self.MyPlug) and IsValid(self.MyPlug.TheirSocket) then -- If we are connected, transfer resources
-		if (self.OtherSocket and self.OtherSocket:IsValid()) then
-			if self.pump_active == 1 then --pump it
-				local energyneeded = math.abs(math.floor(256 / 100 * Energy_Increment))
-				if (energyneeded >= 0) then
-					if (self:GetResourceAmount("energy") >= energyneeded) then
-						local used = self:ConsumeResource( "energy", energyneeded )
-						self.OtherSocket.pump_status = PUMP_ACTIVE
-					elseif (energyneeded > 0) then
-						self.OtherSocket.pump_status = PUMP_NO_POWER
-						rate = 0
-					end
-				end
-			end
-			if rate == 0 then return end
-			for res,v in pairs(self.node.resources) do --actually send it
-				if self.OtherSocket:GetNetworkCapacity(res) > (self.OtherSocket:GetResourceAmount(res) + self.FlowRate) then
-					local amt = self:ConsumeResource(res, self.FlowRate)
-					self.OtherSocket:SupplyResource(res, amt)	
-				else
-					local amt = self.OtherSocket:GetNetworkCapacity(res) - self.OtherSocket:GetResourceAmount(res)
-					amt = self:ConsumeResource(res, amt)
-					self.OtherSocket:SupplyResource(res, amt)	
-				end	
-			end
-		end
-	end
-	
 	if WireAddon then
-		if self.Pumping or IsValid(self.TheirPlug) then
+		local validmyplug = IsValid(self.MyPlug)
+		local validtheirplug = IsValid(self.TheirPlug)
+		if (validmyplug or validtheirplug) and not self.InUse then
 			Wire_TriggerOutput(self, "InUse", 1)
-		else
+			self.InUse = true
+		elseif not validmyplug and not validtheirplug and self.InUse then
 			Wire_TriggerOutput(self, "InUse", 0)
+			self.InUse = false
 		end
 	end
 	
@@ -266,47 +284,80 @@ function ENT:Think()
 end
 
 function ENT:AttachPlug(plug)
-	if not IsValid(plug) then return end
-	
-	-- Set references between them
-	self.TheirPlug = plug
-	self.TheirPlug.TheirSocket = self
-	
-	-- Position plug
-	local phys = plug:GetPhysicsObject()
-		phys:EnableMotion( true )
-		plug:SetPos( self:LocalToWorld( Vector(5,13,10) ) )
-		plug:SetAngles( self:GetAngles() )
-	phys:Wake() --force plug to update
-	
-	-- Constrain together
-	self.Weld = constraint.Weld( self, plug, 0, 0, PLUG_IN_SOCKET_CONSTRAINT_POWER, true, false )
-	self.Weld.Type = "" --prevents the duplicator from making this weld
-	if not IsValid(self.Weld) then
-		self.TheirPlug = nil
-		self.TheirPlug.TheirSocket = nil
-		return
+	if not IsValid(plug) or self.AttachCooldown >= CurTime() then return end
+	if IsValid(self.MyPlug) then
+		if plug == self.MyPlug then return end
 	end
-	self.TheirPlug.Weld = self.Weld
 	
-	-- Prepare clearup incase one is removed
-	plug:DeleteOnRemove( self.Weld )
-	self:DeleteOnRemove( self.Weld )
+	if self.AttachStep==0 then
+		--First, set variables so we know we have a plug we're working with and so their plug can see us
+		self.TheirPlug = plug
+		self.TheirPlug.TheirSocket = self
+		
+		--Next, delete their hose so it doesn't break things later
+		if IsValid(self.TheirPlug.MySocket.Hose) then self.TheirPlug.MySocket.Hose:Remove() end
+		if IsValid(self.TheirPlug.MySocket.rope) then self.TheirPlug.MySocket.rope:Remove() end
+		
+		--Now we create a delay before moving the pump, so the physics engine will ignore the hose and not rip a hole in time space, then rerun ourselves
+		self.AttachStep=1
+		timer.Simple(0.15,function() self:AttachPlug(plug) end)
+		return
+		
+	elseif self.AttachStep==1 then
+		--Second run, now we can safely move and attach the plug
+		-- Position plug
+		local phys = plug:GetPhysicsObject()
+			phys:EnableMotion( true )
+			plug:SetPos( self:LocalToWorld( Vector(5,13,10) ) )
+			plug:SetAngles( self:GetAngles() )
+		phys:Wake() --force plug to update
 	
-	if (self.TheirPlug.MySocket.Hose:IsValid() and self.TheirPlug.MySocket.rope:IsValid())then
+		-- Constrain together
+		self.Weld = constraint.Weld( self, plug, 0, 0, PLUG_IN_SOCKET_CONSTRAINT_POWER, true, false )
+		self.Weld.Type = "" --prevents the duplicator from making this weld
+		if not IsValid(self.Weld) then
+			self.TheirPlug = nil
+			self.TheirPlug.TheirSocket = nil
+			return
+		end
+		self.TheirPlug.Weld = self.Weld
+		
+	
+		-- Prepare cleanup incase one is removed
+		plug:DeleteOnRemove( self.Weld )
+		self:DeleteOnRemove( self.Weld )
+		
+		--Now we create a delay again, so we can tell them to remake their hose
+		self.AttachStep=2
+		timer.Simple(0.15,function() self:AttachPlug(plug) end)
+		return
+		
+	elseif self.AttachStep==2 then
+		--Last run, tell them to remake their hose and run away before something breaks again
+		if not IsValid(self.TheirPlug.MySocket) then
+			timer.Simple(0.15,function() self:AttachPlug(plug) end)
+			return
+		end
+		self.AttachStep=0
+		self.TheirPlug.MySocket:CreateHose()
+		if WireAddon then Wire_TriggerOutput(self, "InUse", 1) end
+	end
+	
+	--[[if (self.TheirPlug.MySocket.Hose:IsValid() and self.TheirPlug.MySocket.rope:IsValid())then
 		local dist = (self.TheirPlug:GetPos() - self.TheirPlug.MySocket:GetPos()):Length()
 		self.TheirPlug.MySocket.ropelength = math.min(dist+100,self.TheirPlug.MySocket.ropemax)
 		
 		self.TheirPlug.MySocket.Hose:Fire("SetSpringLength", self.ropelength, 0)
 		self.TheirPlug.MySocket.rope:Fire("SetLength", self.ropelength, 0)
-	end
+	end]]--	
 	
-	if WireAddon then Wire_TriggerOutput(self, "InUse", 1) end
 end
 
 function ENT:AcceptInput(name,activator,caller)
 	if name == "Use" and caller:IsPlayer() and caller:KeyDownLast(IN_USE) == false then
-		if not self.Receiving and not IsValid(self.MyPlug) then
+		if self.ReelIn then
+			self.ReelIn = false
+		elseif not IsValid(self.Weld) and not IsValid(self.MyPlug) then
 			self:CreatePlug()
 		elseif not self.ReelIn then
 			self:Disconnect()
@@ -317,7 +368,9 @@ end
 function ENT:TriggerInput(iname, value)
 	if iname == "Toggle" then
 		if not self.Toggled and value > 0 then
-			if not self.Receiving and not IsValid(self.MyPlug) then
+			if self.ReelIn then
+				self.ReelIn = false
+			elseif not IsValid(self.Weld) and not IsValid(self.MyPlug) then
 				self:CreatePlug()
 				self.Toggled = true
 			elseif not self.ReelIn then
@@ -340,6 +393,14 @@ function ENT:OnRemove()
 		self.MyPlug:Remove()
 	end
 	self.BaseClass.OnRemove(self)
+end
+
+function ENT:Touch( plug )
+	if not IsValid(self.TheirPlug) then	--skip if we have a plug already
+		if IsValid(plug) and plug.is_plug and not IsValid(plug.TheirSocket) and plug:IsPlayerHolding() == false then --found a plug and not it's not in another socket --player isn't holding the plug spamming connections
+			self:AttachPlug(plug)
+		end
+	end
 end
 
 
